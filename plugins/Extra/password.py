7bc45eefb4a5bdf69909d67b146a3e6fde466669
Maariaaaluis <150658183+Maariaaaluis@users.noreply.github.com>
2023-12-04 12:57:23 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Bot Owner', url='https://t.me/Faheem21025492006')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Bot Owner', url='https://t.me/Alyaralp')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
